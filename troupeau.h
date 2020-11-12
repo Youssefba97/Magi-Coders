3dcd5f1c410a50c6183716498d35e0e5f8f29984
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2020-11-12 17:56:53 +0100

    ajout de l'entete des fonctions pour le nombre de brebis et de veaux

--- troupeau.h
+++ troupeau.h
@@ -3,4 +3,6 @@ void modifier_animal(Troupeau * p_troupeau, int * p_index);
 void supprimer_animal(Troupeau * p_troupeau, int * p_index);
 void chercher_animal(Troupeau * p_troupeau, int * p_index);
 void afficher_troupeaux(Troupeau * p_troupeau, int * p_index);
+int Nombre_brebis(Troupeau * p_troupeau, int * p_index);
+int Nombre_veaux(Troupeau * p_troupeau, int * p_index);
 
