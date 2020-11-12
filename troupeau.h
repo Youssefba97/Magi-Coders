bfc9024a5a9ce16e177176a3091c1e1a860ac3af
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2020-11-12 17:50:59 +0100

    ajout de l'entete de la procedure chercher_troupeau

--- troupeau.h
+++ troupeau.h
@@ -1,4 +1,6 @@
 void ajouter_animal(Troupeau * p_troupeau, int * p_index);
 void modifier_animal(Troupeau * p_troupeau, int * p_index);
 void supprimer_animal(Troupeau * p_troupeau, int * p_index);
+void chercher_animal(Troupeau * p_troupeau, int * p_index);
 void afficher_troupeaux(Troupeau * p_troupeau, int * p_index);
+
