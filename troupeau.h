4dc468d0340a00a7867bbfaee2152db92f7a44ff
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2020-11-12 17:46:48 +0100

    ajout de troupeau.h

---
+++ troupeau.h
@@ -0,0 +1,4 @@
+void ajouter_animal(Troupeau * p_troupeau, int * p_index);
+void modifier_animal(Troupeau * p_troupeau, int * p_index);
+void supprimer_animal(Troupeau * p_troupeau, int * p_index);
+void afficher_troupeaux(Troupeau * p_troupeau, int * p_index);
