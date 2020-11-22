c9dbd671b4d06b2daa58873bee17ec60934b335d
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2020-11-22 18:10:35 +0100

    modification de main.c

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "troupeau.h"
+
 void main()
 {
-int a,b;
-a=5;
-b=2;
-printf("\n a=%d b=%d ",a,b);
+Troupeau troupeau1,troupeau2;
+printf("donner un id\n");
+scanf("%s",troupeau1.identifiant);
+printf("donner un type\n");
+scanf("%s",troupeau1.type);
+printf("donner un sexe\n");
+scanf("%s",troupeau1.sexe);
+printf("donner un etat\n");
+scanf("%d",&troupeau1.etat);
+printf("donner un jour\n");
+scanf("%d",&troupeau1.date.jour);
+printf("donner un mois\n");
+scanf("%d",&troupeau1.date.mois);
+printf("donner une annee\n");
+scanf("%d",&troupeau1.date.annee);
+
+ajouter_animal(troupeau1);
+
 }
